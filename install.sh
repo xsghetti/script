4379fd232023fbc9abd133070ef9bdd6f7aac2db
xSghetti <dmedoro13@gmail.com>
2024-07-16 18:21:56 -0400

    install.sh? maybe?

--- install.sh
+++ install.sh
@@ -2,3 +2,14 @@ pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
+
+sudo pacman -S --needed - < ~/script/pacman.txt
+yay -S - < ~/script/yay.txt
+
+git clone https://github.com/xsghetti/hyprcrux
+
+cp -r ~/hyprcrux/.config/* ~/.config/
+sudo cp -r ~/hyprcrux/src/icons/* /usr/share/icons/
+sudo cp -r ~/hyprcrux/src/fonts/* /usr/share/fonts/
+sudo cp -r ~/hyprcrux/src/themes/* /usr/share/themes/
+

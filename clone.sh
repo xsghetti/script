e95c745d44b0296ae3beb1b4e76711d2d8f5764c
xSghetti <dmedoro13@gmail.com>
2024-07-16 19:04:56 -0400

    install.sh? maybe?

---
+++ clone.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Clone the repository
+git clone https://github.com/xsghetti/hyprcrux
+
+# Copy the configuration files
+cp -r ~/hyprcrux/.config/* ~/.config/
+
+# Copy icons, fonts, and themes to their respective system directories
+sudo cp -r ~/hyprcrux/src/icons/* /usr/share/icons/
+sudo cp -r ~/hyprcrux/src/fonts/* /usr/share/fonts/
+sudo cp -r ~/hyprcrux/src/themes/* /usr/share/themes/
--- install.sh
+++ install.sh
@@ -6,13 +6,7 @@ makepkg -si
 sudo pacman -S --needed - < ~/script/pacman.txt
 yay -S - < ~/script/yay.txt
 
-# Clone the repository
-git clone https://github.com/xsghetti/hyprcrux
+cd ~/script/
+./clone.sh
 
-# Copy the configuration files
-cp -r ~/hyprcrux/.config/* ~/.config/
 
-# Copy icons, fonts, and themes to their respective system directories
-sudo cp -r ~/hyprcrux/src/icons/* /usr/share/icons/
-sudo cp -r ~/hyprcrux/src/fonts/* /usr/share/fonts/
-sudo cp -r ~/hyprcrux/src/themes/* /usr/share/themes/

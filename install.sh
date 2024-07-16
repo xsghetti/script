0e94c148419eab20b99cadee3e2e37c3d4779029
Dante <150515748+xsghetti@users.noreply.github.com>
2024-07-16 18:46:41 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,13 @@ makepkg -si
 sudo pacman -S --needed - < ~/script/pacman.txt
 yay -S - < ~/script/yay.txt
 
+# Clone the repository
 git clone https://github.com/xsghetti/hyprcrux
 
+# Copy the configuration files
 cp -r ~/hyprcrux/.config/* ~/.config/
+
+# Copy icons, fonts, and themes to their respective system directories
 sudo cp -r ~/hyprcrux/src/icons/* /usr/share/icons/
 sudo cp -r ~/hyprcrux/src/fonts/* /usr/share/fonts/
 sudo cp -r ~/hyprcrux/src/themes/* /usr/share/themes/
-

ace1ffed6813112b111667c4ba0a1274c370e2da
xSghetti <dmedoro13@gmail.com>
2024-07-16 21:25:15 -0400

    install.sh? maybe?

--- install.sh
+++ install.sh
@@ -1,12 +1,54 @@
-pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si
+#/bin/bash
+cat << "EOF"
+-----------------------------------------------------------------------
+ ██░ ██▓██   ██▓ ██▓███   ██▀███   ▄████▄   ██▀███   █    ██ ▒██   ██▒
+▓██░ ██▒▒██  ██▒▓██░  ██▒▓██ ▒ ██▒▒██▀ ▀█  ▓██ ▒ ██▒ ██  ▓██▒▒▒ █ █ ▒░
+▒██▀▀██░ ▒██ ██░▓██░ ██▓▒▓██ ░▄█ ▒▒▓█    ▄ ▓██ ░▄█ ▒▓██  ▒██░░░  █   ░
+░▓█ ░██  ░ ▐██▓░▒██▄█▓▒ ▒▒██▀▀█▄  ▒▓▓▄ ▄██▒▒██▀▀█▄  ▓▓█  ░██░ ░ █ █ ▒ 
+░▓█▒░██▓ ░ ██▒▓░▒██▒ ░  ░░██▓ ▒██▒▒ ▓███▀ ░░██▓ ▒██▒▒▒█████▓ ▒██▒ ▒██▒
+ ▒ ░░▒░▒  ██▒▒▒ ▒▓▒░ ░  ░░ ▒▓ ░▒▓░░ ░▒ ▒  ░░ ▒▓ ░▒▓░░▒▓▒ ▒ ▒ ▒▒ ░ ░▓ ░
+ ▒ ░▒░ ░▓██ ░▒░ ░▒ ░       ░▒ ░ ▒░  ░  ▒     ░▒ ░ ▒░░░▒░ ░ ░ ░░   ░▒ ░
+ ░  ░░ ░▒ ▒ ░░  ░░         ░░   ░ ░          ░░   ░  ░░░ ░ ░  ░    ░  
+ ░  ░  ░░ ░                 ░     ░ ░         ░        ░      ░    ░  
+        ░ ░                       ░                                   
+                           The Future Is Now
+                             ©xSghetti 2024 
+-----------------------------------------------------------------------
+EOF
+echo ":: WARNING! This script is meant for Nvidia users only! It installs the nvidia-dkms and nvidia-utils packages"
+read -p "Do you want to continue? (y/n) " answer
 
-sudo pacman -S --needed - < ~/script/pacman.txt
-yay -S --needed - < ~/script/yay.txt
+case $answer in
+  [Yy]* )
+    echo ":: Install Continuing..."
+      #Copy mkinitcpio.conf
+      sudo cp -r ~/script/mkinitcpio.conf /etc/mkinitcpio.conf
 
-cd ~/script/
-./clone.sh
+      #Make /etc/modprobe.d/nvidia.conf
+      sudo touch /etc/modprobe.d/nvidia.conf
+      sudo cp -r ~/script/nvidia.conf /etc/modprobe.d/nvidia.conf
 
+      sudo mkinitcpio -P
 
+      #Download and Install Yay
+      pacman -S --needed git base-devel
+      git clone https://aur.archlinux.org/yay-bin.git
+      cd yay-bin
+      makepkg -si
+
+      #Download and Install packages
+      sudo pacman -S --needed - < ~/script/pacman.txt
+      yay -S --needed - < ~/script/yay.txt
+
+      #Clones Hyprcrux Repo and copies them to their correct locations.
+      cd ~/script/
+      ./clone.sh
+      ;;
+  [Nn]* )
+    echo ":: Exiting..."
+    ;;
+  * )
+    echo "Invalid input. please answer y or n."
+    exit 1
+    ;;
+esac

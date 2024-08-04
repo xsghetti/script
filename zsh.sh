9273b6e0ac254c099b360e431ff4b1c1fcc2f79f
Dante <150515748+xsghetti@users.noreply.github.com>
2024-08-04 15:38:50 -0400

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -6,7 +6,7 @@ SOURCE_DIR=~/hyprcrux/src/
 echo "Installing Zsh, Oh-My-Zsh, and Powerlevel10k..."
 sudo pacman -S --noconfirm zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-cp -r ~/hyprcrux/src/Powerlevel10k ~/.oh-my-zsh/custom/themes/
+cp -r src/powerlevel10k ~/.oh-my-zsh/custom/themes/
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 

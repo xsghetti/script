49717d5bb404cb2a9e85c41ab89eb9d1b444e38d
xSghetti <dmedoro13@gmail.com>
2024-08-04 14:29:06 -0400

    restore zsh

--- zsh.sh
+++ zsh.sh
@@ -6,7 +6,7 @@ SOURCE_DIR=~/hyprcrux/src/
 echo "Installing Zsh, Oh-My-Zsh, and Powerlevel10k..."
 sudo pacman -S --noconfirm zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+cp -r ~/hyprcrux/src/Powerlevel10k ~/.oh-my-zsh/custom/themes/
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 

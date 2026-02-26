070c6b826700f6f77f158cd1702c5c43d2466958
Teez <dmedoro13@gmail.com>
2026-02-26 01:53:27 -0500

    Merge and consolidate all scripts into install.sh

--- grub.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo ":: Installing Grub Theme"1
-sudo cp -r ~/script/src/cyberpunk_arcade/ /usr/share/grub/themes/
-sudo cp -r ~/script/src/grub /etc/default/
-sudo grub-mkconfig -o /boot/grub/grub.cfg
-
